dc8ef008628268c90cef98e60592e47d44eb0c6a
Jaesuk <jaesuk95@outlook.com>
2022-09-04 22:23:28 +0900

    구체 클래스 기반 프록시 - 적용

--- src/main/java/com/example/proxypattern/ProxypatternApplication.java
+++ src/main/java/com/example/proxypattern/ProxypatternApplication.java
@@ -2,6 +2,7 @@ package com.example.proxypattern;
 
 import com.example.proxypattern.config.AppV1Config;
 import com.example.proxypattern.config.AppV2Config;
+import com.example.proxypattern.config.v1_proxy.ConcreteProxyConfig;
 import com.example.proxypattern.config.v1_proxy.InterfaceProxyConfig;
 import com.example.proxypattern.trace.logtrace.LogTrace;
 import com.example.proxypattern.trace.logtrace.ThreadLocalLogTrace;
@@ -14,7 +15,8 @@ import org.springframework.context.annotation.Import;
                                 // 일반적으로 @Configuration 같은 설정 파일을 등록할 때 사용하지만, 스프링 빈을 등록할 떄도 사용할 수 있다.
 
 //@Import({AppV1Config.class,AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "com.example.proxypattern.app")
 //@SpringBootApplication(scanBasePackages = "com.example.proxypattern.app")
 public class ProxypatternApplication {

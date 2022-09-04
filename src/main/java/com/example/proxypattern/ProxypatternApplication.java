dfc593ae8ace460ee35a05aec6b8abd672ac5450
Jaesuk <jaesuk95@outlook.com>
2022-09-04 21:08:29 +0900

    인터페이스 기반 프록시 적용

--- src/main/java/com/example/proxypattern/ProxypatternApplication.java
+++ src/main/java/com/example/proxypattern/ProxypatternApplication.java
@@ -2,20 +2,30 @@ package com.example.proxypattern;
 
 import com.example.proxypattern.config.AppV1Config;
 import com.example.proxypattern.config.AppV2Config;
+import com.example.proxypattern.config.v1_proxy.InterfaceProxyConfig;
+import com.example.proxypattern.trace.logtrace.LogTrace;
+import com.example.proxypattern.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)      // 클래스를 스프링 빈으로 등록한다. 여기서는 AppV1Config.class 를 스프링 빈으로 등록한다.
                                 // 일반적으로 @Configuration 같은 설정 파일을 등록할 때 사용하지만, 스프링 빈을 등록할 떄도 사용할 수 있다.
 
-@Import({AppV1Config.class,AppV2Config.class})
-@SpringBootApplication
+//@Import({AppV1Config.class,AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
+@SpringBootApplication(scanBasePackages = "com.example.proxypattern.app")
 //@SpringBootApplication(scanBasePackages = "com.example.proxypattern.app")
 public class ProxypatternApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProxypatternApplication.class, args);
+    }
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
     }
 
 }
---
+++ src/main/java/com/example/proxypattern/config/v1_proxy/interface_proxy/OrderServiceInterfaceProxy.java
@@ -0,0 +1,27 @@
+package com.example.proxypattern.config.v1_proxy.interface_proxy;
+
+import com.example.proxypattern.app.v1.OrderServiceV1;
+import com.example.proxypattern.trace.TraceStatus;
+import com.example.proxypattern.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderServiceInterfaceProxy implements OrderServiceV1 {
+    private final OrderServiceV1 target;
+    private final LogTrace logTrace;
+
+    @Override
+    public void orderItem(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderService.orderItem()");
+            //target 호출
+            target.orderItem(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}

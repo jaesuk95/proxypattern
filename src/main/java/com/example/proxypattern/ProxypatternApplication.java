d592cbed30a281f9c73328ffd53531b82c42b7c7
Jaesuk <jaesuk95@outlook.com>
2022-09-21 00:13:01 +0900

    JDK dynamic proxy 예제 코드2

--- src/main/java/com/example/proxypattern/ProxypatternApplication.java
+++ src/main/java/com/example/proxypattern/ProxypatternApplication.java
@@ -4,6 +4,7 @@ import com.example.proxypattern.config.AppV1Config;
 import com.example.proxypattern.config.AppV2Config;
 import com.example.proxypattern.config.v1_proxy.ConcreteProxyConfig;
 import com.example.proxypattern.config.v1_proxy.InterfaceProxyConfig;
+import com.example.proxypattern.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import com.example.proxypattern.trace.logtrace.LogTrace;
 import com.example.proxypattern.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,9 +15,9 @@ import org.springframework.context.annotation.Import;
 //@Import(AppV1Config.class)      // 클래스를 스프링 빈으로 등록한다. 여기서는 AppV1Config.class 를 스프링 빈으로 등록한다.
                                 // 일반적으로 @Configuration 같은 설정 파일을 등록할 때 사용하지만, 스프링 빈을 등록할 떄도 사용할 수 있다.
 
-//@Import({AppV1Config.class,AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+
 //@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "com.example.proxypattern.app")
 //@SpringBootApplication(scanBasePackages = "com.example.proxypattern.app")
 public class ProxypatternApplication {
@@ -31,3 +32,6 @@ public class ProxypatternApplication {
     }
 
 }
+
+//@Import({AppV1Config.class,AppV2Config.class})
+//@Import(InterfaceProxyConfig.class)
